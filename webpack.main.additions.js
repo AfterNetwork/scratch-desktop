08c5497dc11451d8e1458f4d052c9592a058a86a
After <45772308+AfterNetwork@users.noreply.github.com>
2019-05-31 21:20:21 -0700

    Activated React.js

--- webpack.main.additions.js
+++ webpack.main.additions.js
@@ -3,6 +3,7 @@ const path = require('path');
 const makeConfig = require('./webpack.makeConfig.js');
 
 module.exports = makeConfig({
+        // Activated React.js here
     name: 'main',
     useReact: true,
     babelPaths: [
